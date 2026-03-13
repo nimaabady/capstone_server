35290e1c0e442aa9c7b0f4522cc37363832f637e
Avano <avanoberlingieri@gmail.com>
2026-03-13 01:44:45 -0400

    Created Rest APIS for all new methods

--- auth/src/main/java/com/capstone/server/auth/controller/AuthController.java
+++ auth/src/main/java/com/capstone/server/auth/controller/AuthController.java
@@ -1,14 +1,13 @@
 package com.capstone.server.auth.controller;
 
-import com.capstone.server.auth.dto.LoginRequestDto;
-import com.capstone.server.auth.dto.LoginResponseDto;
-import com.capstone.server.auth.dto.RegisterRequestDto;
-import com.capstone.server.auth.dto.RegisterResponseDto;
+import com.capstone.server.auth.dto.*;
 import com.capstone.server.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -18,14 +17,50 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<RegisterResponseDto> register(@Valid @RequestBody RegisterRequestDto request) {
+    public ResponseEntity<RegisterResponseDto> register(@Valid @RequestBody RegisterRequestDto dto) {
 
-        return ResponseEntity.ok(authService.registerUser(request));
+        return ResponseEntity.ok(authService.registerUser(dto));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto request) {
+    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto dto) {
 
-        return ResponseEntity.ok(authService.loginUser(request));
+        return ResponseEntity.ok(authService.loginUser(dto));
+    }
+
+    @PostMapping("/logout/{id}")
+    public ResponseEntity<Void> logout(@PathVariable UUID id) {
+
+        authService.logout(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+
+        authService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/password/{id}")
+    public ResponseEntity<Void> updatePassword(@PathVariable UUID id,
+                                               @Valid @RequestBody UpdatePasswordDto dto) {
+
+        authService.changePassword(dto, id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/username/{id}")
+    public ResponseEntity<UpdateResponseDto> changeUsername(@PathVariable UUID id,
+                                                            @Valid @RequestBody UpdateUsernameDto dto) {
+
+        return ResponseEntity.ok(authService.changeUsername(dto, id));
+    }
+
+    @PutMapping("/email/{id}")
+    public ResponseEntity<UpdateResponseDto> changeEmail(@PathVariable UUID id,
+                                                         @Valid @RequestBody UpdateEmailDto dto) {
+
+        return ResponseEntity.ok(authService.changeEmail(dto, id));
     }
 }
