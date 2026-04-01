e8baab1664de48f680188a71cac0baefdeab3db0
Nima Abady <nima.abady@gmail.com>
2026-04-01 16:31:41 -0400

    slight backend rework for friends

--- friends/src/main/java/com/capstone/server/friends/controller/FriendController.java
+++ friends/src/main/java/com/capstone/server/friends/controller/FriendController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/friends")
 @RequiredArgsConstructor
 public class FriendController {
 
